9ba5fd9fb470e1c57be34cc3d58d2fbd31d6e914
Papa <82730987+salartalaA@users.noreply.github.com>
2024-12-30 01:54:36 +0330

    Add files via upload

--- todo_db.sql
+++ todo_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 29, 2024 at 04:06 PM
+-- Generation Time: Dec 29, 2024 at 11:20 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -30,17 +30,18 @@ SET time_zone = "+00:00";
 CREATE TABLE `todos` (
   `id` int(100) NOT NULL,
   `text` varchar(100) NOT NULL,
-  `isCompleted` int(2) NOT NULL DEFAULT 0
+  `isCompleted` int(2) NOT NULL DEFAULT 0,
+  `userID` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
 
 --
 -- Dumping data for table `todos`
 --
 
-INSERT INTO `todos` (`id`, `text`, `isCompleted`) VALUES
-(24, 'Gym', 1),
-(38, 'Coffe', 0),
-(100, 'Code', 1);
+INSERT INTO `todos` (`id`, `text`, `isCompleted`, `userID`) VALUES
+(110, 'Gym', 1, 11),
+(111, 'Coffe', 0, 11),
+(112, 'Code', 1, 11);
 
 -- --------------------------------------------------------
 
@@ -60,7 +61,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
-(8, 'test', 'test@gmail.com', '$2b$10$xhyOzSAOr3hqjwALwppRx.h472Ag62uRadG.ztpYr6KI7mP8ligqu');
+(11, 'salar', 'salarmhmdn@gmail.com', '$2b$10$E4ejW.DWa6fi71RIvBtBW.r1I8yGF/GVhqAg0JVotusmT3K6YdC7e');
 
 --
 -- Indexes for dumped tables
@@ -70,7 +71,8 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
 -- Indexes for table `todos`
 --
 ALTER TABLE `todos`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userID` (`userID`);
 
 --
 -- Indexes for table `users`
@@ -87,13 +89,23 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `todos`
 --
 ALTER TABLE `todos`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=102;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=122;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `todos`
+--
+ALTER TABLE `todos`
+  ADD CONSTRAINT `todos_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
